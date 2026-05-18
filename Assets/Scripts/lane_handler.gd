38fb236878d94a40b05d822ac8bf215422771953
TurbogamesDev <turbogames.mail@gmail.com>
2026-05-18 19:45:02 +0530

    Only spawn notes when they're close to their target time

--- Assets/Scripts/lane_handler.gd
+++ Assets/Scripts/lane_handler.gd
@@ -7,40 +7,52 @@ class_name LaneHandler
 
 var current_parsed_chart: ParsedChart
 
-func spawn_note_in_lane(lane_no: int, target_time: float) -> void:
-    var lane: Lane = lanes[lane_no]
+const NOTE_SPAWN_BUFFER = 2.0
+
+var previous_current_time = 0.0
+
+func spawn_note_in_lane(lane_id: int, target_time: float) -> void:
+    var lane: Lane = lanes[lane_id]
 
     lane.spawn_note(target_time)
 
 func _ready() -> void:
     current_parsed_chart = ParsedChart.init_from_file("res://Assets/Charts/test_chart_1.json")
 
+    for lane: Lane in lanes:
+        var notes_in_lane = current_parsed_chart.get_notes_for_lane_in_timeframe(lane.lane_id, 0.0, NOTE_SPAWN_BUFFER)
+
+        for note_data in notes_in_lane:
+            var target_time = note_data["start_time"]
+
+            spawn_note_in_lane(lane.lane_id, target_time)
+
     # print(parsed_chart.chart_data)
 
-    spawn_note_in_lane(0, 1.00)
-    spawn_note_in_lane(1, 1.25)
-    spawn_note_in_lane(2, 1.50)
-    spawn_note_in_lane(3, 1.75)
+    # spawn_note_in_lane(0, 1.00)
+    # spawn_note_in_lane(1, 1.25)
+    # spawn_note_in_lane(2, 1.50)
+    # spawn_note_in_lane(3, 1.75)
 
-    spawn_note_in_lane(0, 2.00)
-    spawn_note_in_lane(1, 2.25)
-    spawn_note_in_lane(2, 2.50)
-    spawn_note_in_lane(3, 2.75)
+    # spawn_note_in_lane(0, 2.00)
+    # spawn_note_in_lane(1, 2.25)
+    # spawn_note_in_lane(2, 2.50)
+    # spawn_note_in_lane(3, 2.75)
 
-    spawn_note_in_lane(0, 3.00)
-    spawn_note_in_lane(1, 3.25)
-    spawn_note_in_lane(2, 3.50)
-    spawn_note_in_lane(3, 3.75)
+    # spawn_note_in_lane(0, 3.00)
+    # spawn_note_in_lane(1, 3.25)
+    # spawn_note_in_lane(2, 3.50)
+    # spawn_note_in_lane(3, 3.75)
 
-    spawn_note_in_lane(0, 4.00)
-    spawn_note_in_lane(1, 4.25)
-    spawn_note_in_lane(2, 4.50)
-    spawn_note_in_lane(3, 4.75)
+    # spawn_note_in_lane(0, 4.00)
+    # spawn_note_in_lane(1, 4.25)
+    # spawn_note_in_lane(2, 4.50)
+    # spawn_note_in_lane(3, 4.75)
 
-    spawn_note_in_lane(0, 5.00)
-    spawn_note_in_lane(1, 5.25)
-    spawn_note_in_lane(2, 5.50)
-    spawn_note_in_lane(3, 5.75)
+    # spawn_note_in_lane(0, 5.00)
+    # spawn_note_in_lane(1, 5.25)
+    # spawn_note_in_lane(2, 5.50)
+    # spawn_note_in_lane(3, 5.75)
 
     
     
@@ -51,15 +63,25 @@ func _ready() -> void:
     # spawn_note(4.0 + 0.25*lane_id)
     # spawn_note(5.0 + 0.25*lane_id)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
     if not current_parsed_chart:
         return
 
     for lane: Lane in lanes:
-        var notes_in_lane_for_delta_time_period = current_parsed_chart.get_notes_for_lane_in_timeframe(lane.lane_id, ChartTimeSynchroniser.current_time, ChartTimeSynchroniser.current_time + delta)
+        var notes_in_lane_during_change_in_current_time = current_parsed_chart.get_notes_for_lane_in_timeframe(
+            lane.lane_id,
+            previous_current_time + NOTE_SPAWN_BUFFER,
+            ChartTimeSynchroniser.current_time + NOTE_SPAWN_BUFFER
+        )
 
-        if len(notes_in_lane_for_delta_time_period) > 0:
-            print(notes_in_lane_for_delta_time_period)
+        for note_data in notes_in_lane_during_change_in_current_time:
+            var target_time = note_data["start_time"]
+
+            spawn_note_in_lane(lane.lane_id, target_time)
+
+    previous_current_time = ChartTimeSynchroniser.current_time
+
+    
 
 
     
