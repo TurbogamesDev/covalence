5e9b29218e45cfc54dcd07992171f50c4bf91366
TurbogamesDev <turbogames.mail@gmail.com>
2026-05-25 16:55:37 +0530

    Added a hold note which can change tail length, not implemented yet

---
+++ Assets/Scripts/hold_note.gd
@@ -0,0 +1,23 @@
+extends Node2D
+class_name HoldNote
+
+@export var tail_polygon_2d: Polygon2D
+@export var tail_width: float = 144
+
+var tail_length: float = 144
+
+func change_tail_length(new_length: float):
+    self.tail_length = new_length
+    
+    var new_polygon = PackedVector2Array([
+        Vector2(-tail_width / 2, 0),
+        Vector2(-tail_width / 2, -new_length),
+        Vector2(tail_width / 2, -new_length),
+        Vector2(tail_width / 2, 0)
+    ])
+
+    self.tail_polygon_2d.polygon = new_polygon
+
+func _process(_delta: float) -> void:
+    change_tail_length(ChartTimeSynchroniser.current_time * 100)
+
