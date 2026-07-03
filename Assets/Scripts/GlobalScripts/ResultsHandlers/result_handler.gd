b8af86fd7e793151acd51c21fa44d01646ba9fa8
TurbogamesDev <turbogames.mail@gmail.com>
2026-07-03 20:45:44 +0530

    Separate displaying current result into helper function

--- Assets/Scripts/GlobalScripts/ResultsHandlers/result_handler.gd
+++ Assets/Scripts/GlobalScripts/ResultsHandlers/result_handler.gd
@@ -5,6 +5,9 @@ func add_judgement_data_to_result(judgement_data: JudgementData) -> void:
 
     ComboHandler.update_combo_from_judgement_data(judgement_data)
 
+    display_current_result()
+
+func display_current_result():
     print(AccuracyHandler.get_current_accuracy())
 
     print("Combo: %d" % ComboHandler.current_combo)
